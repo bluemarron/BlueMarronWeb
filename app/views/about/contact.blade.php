689627481243ecfe61ed919c7acaa09d119ca661
bluemarron <jack.bluemarron@gmail.com>
2013-05-22 00:41:48 +0900

    .

--- app/views/about/contact.blade.php
+++ app/views/about/contact.blade.php
@@ -4,7 +4,7 @@
   <div class="well">
     <h3>Contact</h3>
     <p>
-      jack.bluemarron<img src="/l4/public/images/at_sign.png" width="12">gmail.com
+      jack.bluemarron<img src="/images/at_sign.png" width="12">gmail.com
     </p>
   </div>
 @stop
